1d37c0f06c489db56b232b56df2d880bd82a9711
Matt <mjaywoods@gmail.com>
2017-04-04 14:56:36 +0100

    Update app.php

--- web/app.php
+++ web/app.php
@@ -1,7 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-
+ini_set('date.timezone', 'Europe/London');
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 $kernel = new AppKernel('prod', false);
